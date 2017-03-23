69a4c0587b3170d05f769d2b7577c9e4f9960787
Will James <wjames@thoughtworks.com>
2017-03-23 19:42:58 +1100

    Merge ci and ecs templates

--- deploy-lambci-ecs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-
-aws cloudformation deploy --stack-name lambci-ecs --template-file ecs.ci.template.json --capabilities CAPABILITY_NAMED_IAM
--- spinup-ci-cluster.sh
+++ spinup-ci-cluster.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-aws cloudformation deploy --stack-name lambci-ecs --template-file ecs.ci.template.json --capabilities CAPABILITY_NAMED_IAM --parameter-overrides "DesiredInstanceCapacity=1"
+AS_GROUP_NAME=$(aws cloudformation describe-stacks --stack-name lambci --query 'Stacks[0].Outputs[?OutputKey==`AutoScalingGroupName`].OutputValue' --output text)
+
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $AS_GROUP_NAME --desired-capacity 1
