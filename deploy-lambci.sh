c3e1759f519bbfac3891ce1bb658c6cdb85b85e1
Will James <wjames@thoughtworks.com>
2017-06-13 21:32:02 +1000

    Will need aws cli present

--- deploy-lambci.sh
+++ deploy-lambci.sh
@@ -6,6 +6,8 @@ set -e
 source ./config/env.sh
 unset AWS_SESSION_TOKEN
 
+pip install awscli
+
 REPOS="sportnumerics/infra,sportnumerics/stats,sportnumerics/predict,sportnumerics/explorer-api,sportnumerics/explorer"
 
 aws cloudformation deploy --stack-name lambci --template-file ci.template.json --parameter-overrides "Repositories=$REPOS" --capabilities CAPABILITY_NAMED_IAM
