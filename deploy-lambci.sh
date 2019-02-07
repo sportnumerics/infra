1996caea5d850e20c647d2c608069573b177b197
Will James <jameswt@gmail.com>
2019-02-06 21:57:31 -0600

    Need to install awscli

--- deploy-lambci.sh
+++ deploy-lambci.sh
@@ -4,5 +4,8 @@ set -e
 
 ./decrypt.sh
 source ./config/env.sh
+unset AWS_SESSION_TOKEN
+
+pip install --user awscli
 
 aws cloudformation deploy --stack-name lambci --template-file ci.template.yaml --parameter-overrides "GithubToken=$GITHUB_TOKEN" "GithubSecret=$GITHUB_SECRET" "SlackToken=$SLACK_TOKEN" --capabilities CAPABILITY_NAMED_IAM
