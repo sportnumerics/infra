777754194665e5f14f46d0b51eea717b5da8b63c
Will James <jameswt@gmail.com>
2020-01-28 22:38:28 -0600

    Add docker build args to lambci template for sportnumerics/stats

--- deploy-lambci.sh
+++ deploy-lambci.sh
@@ -2,4 +2,34 @@
 
 set -e
 
-aws cloudformation deploy --stack-name lambci --template-file ci.template.yaml --parameter-overrides "GithubToken=$GITHUB_TOKEN" "GithubSecret=$GITHUB_SECRET" "SlackToken=$SLACK_TOKEN" --capabilities CAPABILITY_NAMED_IAM
+STACK_NAME=lambci
+
+aws cloudformation deploy --stack-name $STACK_NAME --template-file ci.template.yaml --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+
+AUTOSCALING_GROUP_NAME=$(
+  aws cloudformation describe-stacks \
+    --stack-name $STACK_NAME \
+    --query 'Stacks[0].Outputs[?OutputKey==`AutoScalingGroupName`].OutputValue' \
+    --output text
+  )
+
+CLUSTER=$(
+  aws cloudformation describe-stacks \
+    --stack-name $STACK_NAME \
+    --query 'Stacks[0].Outputs[?OutputKey==`Cluster`].OutputValue' \
+    --output text
+  )
+
+TASK=$(
+  aws cloudformation describe-stacks \
+    --stack-name $STACK_NAME \
+    --query 'Stacks[0].Outputs[?OutputKey==`Task`].OutputValue' \
+    --output text
+  )
+
+PROJECT="gh/sportnumerics/stats"
+
+lambci config --project $PROJECT docker.autoscalingGroup $AUTOSCALING_GROUP_NAME
+lambci config --project $PROJECT docker.cluster $CLUSTER
+lambci config --project $PROJECT docker.task $TASK
+lambci config --project $PROJECT docker.runArgs "-v /var/run/docker.sock:/var/run/docker.sock --ulimit nofile=262144:262144"
